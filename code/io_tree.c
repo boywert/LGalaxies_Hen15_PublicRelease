b65bf6371030134cab4cabe5d04242d308ed30d1
Chaichalit Srisawat <boywert@gmail.com>
2016-01-29 15:31:48 +0000

    Add HDF5 merger tree input

--- code/io_tree.c
+++ code/io_tree.c
@@ -243,13 +243,13 @@ void free_tree_table(void)
 #ifdef UPDATETYPETWO
   myfree(TreeAuxData);
 #endif
-  printf("test 1\n");
 #ifndef HDF5_INPUT
 #ifdef LOADIDS
   fclose(treedbids_file);
 #endif
   fclose(tree_file);
 #endif
+  printf("test 1\n");
 }
 
 
